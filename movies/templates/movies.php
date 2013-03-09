28ad91d5d1b4cbca2bd9ff8220806c2c84a7f253
root <root@oc.(none)>
2013-03-09 14:52:10 +0100

    a few changes. not working yet

--- movies/templates/movies.php
+++ movies/templates/movies.php
@@ -11,6 +11,7 @@ if(empty($_['list'])) {
 	foreach($_['list'] as $entry) {
 		echo('
 		<tr>
+			<td width="1"><img align="left" src="'.\OCP\Util::linkToAbsolute('movies','ajax/thumbnail.php').'?filepath='.urlencode($entry['url']).'"></td>
 			<td width="1"><img align="left" src="'.\OCP\Util::linkToAbsolute('movies','img/movies.png').'"></td>
 			<td><a target="_blank" href="'.\OCP\Util::linkToAbsolute('files','download.php').'&file='.urlencode($entry['url']).'&name='.urlencode($entry['name']).'">'.$entry['name'].'</a></td>
 			<td>'.\OCP\Util::formatDate($entry['mtime']).'</td><td>'.\OCP\Util::humanFileSize($entry['size']).'</td>
